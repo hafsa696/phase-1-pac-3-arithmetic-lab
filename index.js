2a574b0a8715d343c74f4e92e3dd9005f17d50b8
hafsa696 <hafwirakullow@gmail.com>
2024-12-25 14:31:08 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+    return "something";
+  }
+  function subtract(a,b) {
+    return "something"
+  }
+  function multiply(a,b) {
+    return"something"
+    
+  }
+  function divide(a,b) {
+    return"something"  
+  }
+  function add(a,b) {
+    return a+b
+  }
+  function subtract(a,b) {
+    return a-b  
+  }
+  function multiply(a,b) {
+    return a*b   
+  }
+  function divide(a,b) {
+    return a/b  
+  }
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  function preserveDecimal(n) {
+    return parseFloat (2.222)  
+  }
+  function preserveDecimal(n) {
+    return NaN
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n); 
+  }
+  
